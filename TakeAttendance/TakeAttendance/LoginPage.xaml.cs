f2b2af85dd4a2a3f4c7b8b315574bb081073d61c
John Dang <vandangjohn@outlook.com>
2022-05-07 01:41:17 -0700

    Updated UI and Navigation

---
+++ TakeAttendance/TakeAttendance/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TakeAttendance
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(username.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(password.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                //Display message to have users fill out fields
+                Console.WriteLine("Empty field");
+
+            }
+            else //Navigate to Home Page
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
